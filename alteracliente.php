a67c80c61af8fd6e1bab542b9daef1e6a74cf4bc
Mirella Morigene de Souza <mimimorigene2010@gmail.com>
2023-06-28 08:59:42 -0300

    Consertando falhas do Altera Ciente.
    Mudança de values para puxar do banco.

--- alteracliente.php
+++ alteracliente.php
@@ -14,6 +14,11 @@ while ($tbl = mysqli_fetch_array($retorno)) {
     $nome = $tbl[2]; #CAMPO NOME DA TABELA DO BANCO
     $cpf = $tbl[1]; #CAMPO CPF DA TABELA DO BANCO
     $senha = $tbl[3]; #CAMPO SENHA DA TABELA DO BANCO
+    $datanasc = $tbl[4]; #CAMPO DATANASC DA TABELA DO BANCO
+    $telefone = $tbl[5]; #CAMPO TELEFONE DA TABELA DO BANCO
+    $logradouro = $tbl[6]; #CAMPO LOGRADOURO DA TABELA DO BANCO
+    $numero = $tbl[7]; #CAMPO NUMERO DA TABELA DO BANCO
+    $cidade = $tbl[8]; #CAMPO CIDADE DA TABELA DO BANCO
     $ativo = $tbl[9]; #CAMPO ATIVO DA TABELA DO BANCO
 }
 
@@ -23,9 +28,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['nome'];
     $cpf = $_POST['cpf'];
     $senha = $_POST['senha'];
+    $datanasc = $_POST['datanasc'];
+    $telefone = $_POST['telefone'];
+    $logradouro = $_POST['logradouro'];
+    $numero = $_POST['numero'];
+    $cidade = $_POST['cidade'];
     $ativo = $_POST['ativo'];
 
-    $sql = "UPDATE clientes SET cli_nome = '$nome', cli_cpf = '$cpf', cli_senha = '$senha', cli_ativo = '$ativo' WHERE cli_id = $id";
+    $sql = "UPDATE clientes SET cli_nome = '$nome', cli_cpf = '$cpf', cli_senha = '$senha', cli_datanasc = STR_TO_DATE('$datanasc','%Y-%m-%d'), cli_telefone = '$telefone', cli_logradouro = '$logradouro', cli_numero = '$numero', cli_cidade = '$cidade', cli_ativo = '$ativo' WHERE cli_id = $id";
 
     mysqli_query($link,$sql);
 
@@ -60,11 +70,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <!--placeholder é o nome que vai aparecer dentro do text do input-->
 
             <input type="hidden" name="id" value="<?=$id?>">
-            <input type="text" name="nome" id="nome" value="<?= $nome ?>" required>
+            <input type="text" name="nome" id="nome" value="<?=$nome ?>" required>  <!-- dentro do php ele vai puxar a informação do banco -->
             <br>
-            <input type="number" name="cpf" id="cpf" value="<?= $cpf ?>" required>
+            <input type="number" name="cpf" id="cpf" value="<?=$cpf ?>" required>
             <br>
-            <input type="password" name="senha" id="senha" value="<?= $senha?>" required>
+            <input type="password" name="senha" id="senha" value="<?=$senha?>" required>
+            <br>
+            <input type="date" name="datanasc" id="datanasc" value="<?=$datanasc ?>" required>
+            <br>
+            <input type="number" name="telefone" id="telefone" value="<?=$telefone ?>" required>
+            <br>
+            <input type="text" name="logradouro" id="logradouro" value="<?=$logradouro ?>" required>
+            <br>
+            <input type="number" name="numero" id="numero" value="<?=$numero ?>" required>
+            <br>
+            <input type="text" name="cidade" id="cidade" value="<?=$cidade ?>" required>
             <br>
             <input type="radio" name="ativo" value="s" <?=$ativo=="s"?"checked":""?>>ATIVO
             <br>
