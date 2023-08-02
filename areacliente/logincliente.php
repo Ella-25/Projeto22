9832961b90e59772474d7d8929632d76d5d0a518
Mirella Morigene de Souza <mimimorigene2010@gmail.com>
2023-08-02 08:42:29 -0300

    Atualização de tela Login Cliente da Área Cliente.

--- areacliente/logincliente.php
+++ areacliente/logincliente.php
@@ -1,31 +1,38 @@
 <?php
+
 session_start();
 include("../conectadb.php");
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $cpf = $_POST['cpf'];
-    $senha = $_POST['senha'];
-
-    $sql = "SELECT COUNT(cli_id) FROM clientes WHERE cli_cpf = '$cpf' AND cli_senha = '$senha'";
-    $retorno = mysqli_query($link, $sql);
-    while($tbl = mysqli_fetch_array($retorno)){
-        $cont = $tbl[0];
-    }
-
-    //VALIDA SE É REALMENTE O CLIENTE PARA LOGIN
-    if($cont == 1){
-        $sql = "SELECT * FROM clientes WHERE cli_cpf = '$cpf' AND cli_senha = '$senha' AND cli_ativo = 's'";
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $cpf = $_POST['cpf'];
+        $senha = $_POST['senha'];
+        $sql = "SELECT COUNT(cli_id) FROM clientes WHERE cli_cpf = '$cpf' AND cli_senha = '$senha'";
         $retorno = mysqli_query($link, $sql);
-        while($tbl = mysqli_fetch_array($retorno)){
-            $_SESSION['idcliente'] = $tbl[0];
-            $_SESSION['nomecliente'] = $tbl[2];
+
+        while($tbl = mysqli_fetch_array($retorno))
+        {
+            $cont = $tbl[0];
         }
-        echo"<script>window.alert.location.href='loja.php';</script>";
+
+        //VALIDA SE É REALMENTE O CLIENTE PARA LOGIN
+        if($cont == 1)
+        {
+            $sql = "SELECT * FROM clientes WHERE cli_cpf = '$cpf' AND cli_senha = '$senha' AND cli_ativo = 's'";
+            $retorno = mysqli_query($link, $sql);
+
+            while($tbl = mysqli_fetch_array($retorno))
+            {
+                $_SESSION['idcliente'] = $tbl[0];
+                $_SESSION['nomecliente'] = $tbl[2];
+            }
+            echo"<script>window.location.href='loja.php';</script>";
+        }
+        else
+        {
+            echo"<script>window.alert('USUARIO OU SENHA INCORRETOS');</script>";
+        }
     }
-    else{
-        echo"<script>window.alert('USUÁRIO OU SENHA INCORRETOS');</script>";
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -38,10 +45,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 </head>
 <body>
     <form action="logincliente.php" method="post">
-        <h1>LOGIN DE CLIENTES</h1>
+        <h1>LOGIN DE CLIENTE</h1>
         <input type="text" name="cpf" id="cpf" placeholder="CPF">
         <br>
-        <input type="password" id="senha" name="senha" placeholder="SENHA">
+        <input type="password" name="senha" id="senha" placeholder="SENHA">
         <br>
         <input type="submit" name="login" value="LOGIN">
     </form>
